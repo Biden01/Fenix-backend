9d82678b048905eafc9ad8e13cb1abb496f935a7
Erasyl <erasylbidan@mail.ru>
2026-01-10 03:03:09 +0500

    fix cors

--- app/main.py
+++ app/main.py
@@ -24,6 +24,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
+        "http://localhost:4173",
+        "http://localhost:8082",
         "http://127.0.0.1:5173",
         "http://localhost:3000",
         "http://127.0.0.1:3000",
